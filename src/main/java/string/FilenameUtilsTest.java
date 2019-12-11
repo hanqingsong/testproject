9e2c863e7e7e6cecba960cc8d2f151dcc155e4e5
hanqingsong <842071912@qq.com>
2019-12-11 09:22:12 +0800

    FilenameUtils使用

--- src/main/java/string/FilenameUtilsTest.java
+++ src/main/java/string/FilenameUtilsTest.java
@@ -16,12 +16,13 @@ public class FilenameUtilsTest {
         String name = FilenameUtils.getName(url);
         System.out.println(name);
 
-        //返回文件名，不包含后缀名
+        //返回文件名，不包含后缀名 12d2815296514e5ca9f2ce56d9a88bce
         String baseName = FilenameUtils.getBaseName(url);
         System.out.println(baseName);
 
-        //返回文件后缀名
+        //返回文件后缀名 jpg
         String extension = FilenameUtils.getExtension(url);
         System.out.println(extension);
+
     }
 }
